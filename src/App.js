1093082d5a4c1268de94ae8cf508fa072dbb862c
dominykas577 <77943502+dominykas577@users.noreply.github.com>
2021-02-25 12:42:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Coin from './Components/Coin/coin';
+import logo from './logo.svg';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={logo} alt="React logo" className="App-logo"/>
+        <h1 className="App-title">
+         Coin Marketcap
+        </h1>
+         </header>
+         <table className="cointable">
+      <thead className="thead-row">
+        <tr>
+          <th>Name</th>
+          <th>Ticker</th>
+          <th>Price</th>
+        </tr>
+      </thead> 
+      <tbody>
+        
+        <Coin name="Bitcoin" ticker="BTC" price = {800}/>
+        <Coin name="Ethereum" ticker="ETH" price= {200}/>
+        <Coin name="Tether" ticker="USDT" price= {1}/>
+        <Coin name="Ripple" ticker="XRP" price= {0.5}/>
+
+
+      
+        <Coin name="Binance Coin" ticker="BNB" price = {800}/>
+        <Coin name="Ethereum" ticker="ETH" price= {200}/>
+        <Coin name="Tether" ticker="USDT" price= {1}/>
+        <Coin name="Ripple" ticker="XRP" price= {0.5}/>
+      </tbody>
+    </table>
     </div>
   );
 }
 
 export default App;
+
