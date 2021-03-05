5b6869a4dffdd768aa6765af4dad5474f0a9928a
dominykas577 <dompoz@gmail.com>
2021-03-05 13:41:51 +0100

    added some chanhges

--- src/App.js
+++ src/App.js
@@ -9,46 +9,75 @@ import AccountBalance from './Components/AccountBalance/AccountBalance.jsx'
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React logo" className="App-logo"/>
-        <h1 className="App-title">
-         Coin Marketcap
-        </h1>
-         </header>
-         <AccountBalance amount={10000} />
-         <table className="cointable">
-      <thead className="thead-row">
-        <tr>
-          <th>Name</th>
-          <th>Ticker</th>
-          <th>Price</th>
-        </tr>
-      </thead> 
-      <tbody>
 
-       
-        
-        <Coin name="Bitcoin" ticker="BTC" price = {800}/>
-        <Coin name="Ethereum" ticker="ETH" price= {200}/>
-        <Coin name="Tether" ticker="USDT" price= {1}/>
-        <Coin name="Ripple" ticker="XRP" price= {0.5}/>
-      
-       
+class App extends React.Component() {
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        {
+          name: 'Bitcoin',
+          ticker: 'BTC',
+          price: 800
+        },
+        {
+          name: 'Ethereum',
+          ticker: 'ETH',
+          price: 200
+        },
+        {
+          name: 'Tether',
+          ticker: 'USDT',
+          price: 1
+        },
+        {
+          name: 'Ripple',
+          ticker: 'XRP',
+          price: 0.5
+        }
+      ]
+    }
+  }
 
-      
-        <Coin name="Binance Coin" ticker="BNB" price = {800}/>
-        <Coin name="Ethereum" ticker="ETH" price= {200}/>
-        <Coin name="Tether" ticker="USDT" price= {1}/>
-        <Coin name="Ripple" ticker="XRP" price= {0.5}/>
 
-      
-      </tbody>
-    </table>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="React logo" className="App-logo"/>
+          <h1 className="App-title">
+           Coin Marketcap
+          </h1>
+           </header>
+           <AccountBalance amount={this.state.balance} />
+           <table className="cointable">
+        <thead className="thead-row">
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead> 
+        <tbody>
+          <Coin name={this.state.coinData[0].name} 
+                ticker={this.state.coinData[0].ticker} 
+                price ={this.state.coinData[0].price}/>
+          <Coin name={this.state.coinData[1].name} 
+                ticker={this.state.coinData[1].ticker} 
+                price ={this.state.coinData[1].price}/>
+          <Coin name={this.state.coinData[2].name} 
+                ticker={this.state.coinData[2].ticker} 
+                price ={this.state.coinData[2].price}/>
+          <Coin name={this.state.coinData[3].name} 
+                ticker={this.state.coinData[3].ticker} 
+                price ={this.state.coinData[3].price}/>
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
